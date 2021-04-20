25da27ebc6e63e7a889c19b50024b9323517a899
Yvette Tan <yvettetan97@gmail.com>
2021-04-21 01:45:16 +0800

    add file

---
+++ MomentumApp/assets/index.js
@@ -0,0 +1,23 @@
+
+// time
+const time = document.getElementById('time');
+
+let now = new Date();
+const currentTime = new Intl.DateTimeFormat('default',
+    {
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric'
+    }).format(now);
+
+time.innerHTML = currentTime;
+
+let userName;
+
+function getUserName() {
+    const continueBtn = document.getElementById('continue-button');
+    const nameInput = document.getElementById('name').value;
+    userName = nameInput;
+    return userName;
+};
+
