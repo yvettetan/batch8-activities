51c6b681671167ad21764d11eeba3959bc34b95d
Yvette Tan <yvettetan97@gmail.com>
2021-05-22 00:36:17 +0800

    handles errors

--- BankApp/assets/index.js
+++ BankApp/assets/index.js
@@ -1,29 +1,20 @@
-//todo wrong_arguments (e.g. amount cannot be negative, name cannot start with a number)
-//todo user_already_exists ('Den' == 'den')
-//? user_does_not_exists ('Den' == 'den')
-
-//todo update table UI when storage updates
-//todo show transaction history with date
-
-//todo sender not enought balance
-
-//display user's name on page load
-window.addEventListener('load', () => {
-    let params = (new URL(document.location)).searchParams;
-    const name = params.get('name');
-    //display user's name
-    document.querySelector('#main-user-name').textContent = name;
-})
+//todo wrong_arguments (e.g. name cannot start with a number)
+//todo update table UI for deposit, withdraw, send updates
+//todo create alert message for new account & delete account
+//todo use helper functions for each form
 
 /*****ELEMENTS*****/
 const dashboard = document.querySelector('#dashboard');
 const accounts = document.querySelector('#accounts');
 const budget = document.querySelector('#budget');
+const currentDate = document.querySelector('#date');
+const userAccountModal = document.querySelector('.user-account-modal');
 
-//* header 
+//* HEADER  
 const dashboardBtn = document.querySelector('#dashboard-btn');
 const accountsBtn = document.querySelector('#accounts-btn');
 const budgetBtn = document.querySelector('#budget-btn');
+const userIcon = document.querySelector('#main-user-icon');
 
 dashboardBtn.addEventListener('click', () => {
     active(dashboardBtn);
@@ -43,8 +34,31 @@ budgetBtn.addEventListener('click', () => {
     show(budget);
     hide([dashboard, accounts]);
 })
+userIcon.addEventListener('click', () => {
+    userAccountModal.classList.toggle('hide');
+})
 
-//*dashboard 
+const today = new Date();
+var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+currentDate.textContent = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+
+let alertName;
+//display user's name on page load
+window.addEventListener('load', () => {
+    let params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    //display user's name
+    document.querySelector('#main-user-name').textContent = name;
+    alertMessage.textContent = `Welcome, ${name}`;
+    alertName = name;
+})
+
+//*DASHBOARD TAB 
+const alertBox = document.querySelector('.alert-container');
+const alertMessage = document.querySelector('.alert-message');
+const removeAlert = document.querySelector('#db-remove-alert');
+
 const dbDeposit = document.querySelector('#db-deposit');
 const depositModal = document.querySelector('.deposit-modal-bg');
 const dbWithdraw = document.querySelector('#db-withdraw');
@@ -52,7 +66,12 @@ const withdrawModal = document.querySelector('.withdraw-modal-bg');
 const dbSendMoney = document.querySelector('#db-send-money');
 const sendModal = document.querySelector('.send-modal-bg');
 const dbAddAccount = document.querySelector('#db-add-account');
-const alertBox = document.querySelector('.alert-container');
+
+removeAlert.addEventListener('click', () => {
+    alertMessage.textContent = `Welcome, ${alertName}`;
+    alertBox.style.backgroundColor = 'var(--black)';
+    removeAlert.style.display = 'none';
+})
 
 dbDeposit.addEventListener('click', () => {
     showModal(depositModal);
@@ -77,7 +96,7 @@ dbAddAccount.addEventListener('click', () => {
     hide([dashboard, accountList]);
     hideModal([sendModal]);
 })
-//*accounts 
+//*ACCOUNTS TAB 
 const accListAccounts = document.querySelector('#acc-list-accounts');
 const accAddAccount = document.querySelector('#acc-add-account');
 
@@ -95,7 +114,7 @@ accAddAccount.addEventListener('click', () => {
 })
 
 //* TOGGLE DISPLAYS 
-active = element => {
+active = (element) => {
     element.classList.add('active');
 }
 inactive = elementsArr => {
@@ -122,19 +141,24 @@ hideModal = elementsArr => {
 
 //*USER CLASS 
 class Account {
-    constructor(accountNumber, name, balance, transactions) {
+    constructor(accountNumber, name, dateOfBirth, balance) {
         this.accountNumber = accountNumber;
         this.name = name;
+        this.dateOfBirth = dateOfBirth;
         this.balance = balance;
-        this.transactions = transactions;
     };
 }
 
 //* STORAGE 
+//local storage stores key value pairs
+/*cant store objects in local storage. it has to be a string so need to stringify 
+before adding to storage and parse when pulling it out*/
+//'static' used to be able to call them directly without having to instantiate the store class
+//accounts will be a string version of the entire array of accounts list
 class AccountStore {
     static getAccouts() {
         let accounts;
-        //check if there is an item called accounts, set it to null:
+        //check if there is an item called accounts
         if (localStorage.getItem('accounts') === null) {
             accounts = [];
         } else {
@@ -143,14 +167,12 @@ class AccountStore {
         }
         return accounts;
     }
-
     static addAccount(account) {
         const accounts = AccountStore.getAccouts();
         accounts.push(account);
         //reset to local storage
         localStorage.setItem('accounts', JSON.stringify(accounts));
     }
-
     static deposit(accountNumber, amount) {
         const accounts = AccountStore.getAccouts();
         accounts.some((account) => {
@@ -183,6 +205,18 @@ class AccountStore {
         })
         localStorage.setItem('accounts', JSON.stringify(accounts));
     }
+
+    static deleteAccount(accountNumber) {
+        const accounts = AccountStore.getAccouts();
+        //loop through the accounts and check if current account iterated matches the index
+        accounts.forEach((account, index) => {
+            if (account.accountNumber === accountNumber) {
+                accounts.splice(index, 1);
+            }
+        });
+        //reset to local storage
+        localStorage.setItem('accounts', JSON.stringify(accounts));
+    }
 }
 
 //* ACCOUNT UI 
@@ -200,10 +234,11 @@ class AccountUI {
         //create trow to insert to tbody
         const row = document.createElement('tr');
         row.innerHTML = `
-                <td>${account.accountNumber}</td>
+                <td> ${account.accountNumber.toString().match(/.{1,4}/g).join(' ')}</td>
                 <td>${account.name}</td>
-                <td>₱${account.balance}</td>
-                <td><a href="#">View Transactions</a></td>`;
+                <td>${account.dateOfBirth}</td>
+                <td>₱${account.balance.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</td>
+                <td><button class="fas fa-trash delete"></button></td>`;
         accountlist.appendChild(row);
     }
     //todo update row balance after deposit
@@ -218,9 +253,18 @@ class AccountUI {
 
     //todo update table row balance after money transfer
 
+    //make sure that target element contains the class of delete then delete the whole account row
+    static deleteAccount(target) {
+        if (target.classList.contains('delete')) {
+            //remove parent of parent element (tr) from the DOM
+            target.parentElement.parentElement.remove();
+        }
+    }
     static clear_inputs() {
         document.querySelector("#firstName").value = '';
         document.querySelector("#lastName").value = '';
+        document.querySelector("#middleName").value = '';
+        document.querySelector("#dateOfBirth").value = '';
         document.querySelector('#account-number-input').value = '';
         document.querySelector('#deposit-account-input').value = '';
         document.querySelector('#deposit-amount-input').value = '';
@@ -232,29 +276,44 @@ class AccountUI {
     }
 }
 
-//*EVENT: Display all Accounts 
+//*DISPLAY ALL ACCOUNTS 
 //as soon as the DOM loads, display current accounts list
 document.addEventListener('DOMContentLoaded', AccountUI.list_users);
 
-//*EVENT: create a new account 
+//*CREATE NEW ACCOUNT 
 const addAccountForm = document.querySelector('#add-account-form');
 addAccountForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let randNumber = Math.floor(Math.random() * 900000000000) + 100000000000;
     const firstName = document.querySelector('#firstName').value;
     const lasttName = document.querySelector('#lastName').value;
-    const fullName = `${firstName} ${lasttName}`;
-    create_user(randNumber, fullName, 0);
-    //todo error handling user already exists
-    //todo how to not repeat random account number with exactly 12 numbers
+    const middleName = document.querySelector('#middleName').value;
+    const fullName = `${firstName} ${middleName} ${lasttName} `;
+    const dateOfBirth = document.querySelector('#dateOfBirth').value;
 
+    let existingAccount;
+    const accounts = AccountStore.getAccouts();
+    //check if account already exists
+    accounts.forEach(account => {
+        if (account.name.toLowerCase() === fullName) {
+            if (account.dateOfBirth === dateOfBirth) {
+                existingAccount = true;
+            }
+        }
+    })
+    if (existingAccount) {
+        alert('Account already exists!');
+    } else {
+        create_user(randNumber, fullName, dateOfBirth, 0);
+    }
+    //todo how to not repeat random account number with exactly 12 numbers
+    existingAccount = false;
 });
 
-create_user = (accountNumber, name, balance) => {
-    //todo check if user already exists
+create_user = (accountNumber, name, dateOfBirth, balance) => {
     //instantiate a new account from the Account class
-    const account = new Account(accountNumber, name, balance);
-    alert(`New Account: ${accountNumber} - ${name} - ${balance} `);
+    const account = new Account(accountNumber, name.toUpperCase(), dateOfBirth, balance);
+    alert(`New Account: ${accountNumber} - ${name.toUpperCase()} - ${balance} `);
     //display new account in list
     AccountUI.addAccount(account);
     //add new account to storage
@@ -283,7 +342,7 @@ displayBalanceForm.addEventListener('submit', (e) => {
     Account.get_balance(accountNumberValue);
 })
 
-Account.get_balance = function (accountNumber) {
+Account.get_balance = (accountNumber) => {
     accountNumber = Number(accountNumber);
     let getAccountNumber;
     let balance;
@@ -292,12 +351,13 @@ Account.get_balance = function (accountNumber) {
     accounts.forEach(account => {
         if (account.accountNumber === accountNumber) {
             getAccountNumber = accountNumber;
-            balance = account.balance;
+            //display balance with comma
+            balance = account.balance.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
             name = account.name;
         };
-    })
+    });
     if (getAccountNumber) {
-        balanceDisplay.innerText = `₱${balance}`;
+        Number.isInteger(balance) ? balanceDisplay.innerText = `₱${balance}.00` : balanceDisplay.innerText = `₱${balance}`;
         nameDisplay.innerText = name;
     } else {
         //check if length is just wrong or if account really does not exist
@@ -331,6 +391,7 @@ depositAccountInput.addEventListener('input', () => {
             depositName.innerText = 'Account Not Found';
         };
     }
+    depositAccountName = undefined;
 })
 //event listener on form submit
 const depositForm = document.querySelector('#deposit-modal-form');
@@ -340,7 +401,7 @@ depositForm.addEventListener('submit', (e) => {
     let depositAmount = document.querySelector('#deposit-amount-input').value;
     Account.deposit(accountNumberValue, depositAmount);
 })
-Account.deposit = function (accountNumber, amount) {
+Account.deposit = (accountNumber, amount) => {
     accountNumber = Number(accountNumber);
     amount = parseFloat(amount);
     let depositAccount;
@@ -352,7 +413,9 @@ Account.deposit = function (accountNumber, amount) {
     }
     if (depositAccount) {
         depositName.innerText = "";
-        alert(`Successfully deposited ₱${amount} to ${depositAccount}`);
+        alertMessage.textContent = `Successfully Deposited ₱${amount} to Account Number: ${depositAccount}`;
+        success();
+        // alert(`Successfully deposited ₱${amount} to ${depositAccount} `);
         // update account balance in local storage
         AccountStore.deposit(accountNumber, amount);
         AccountUI.clear_inputs();
@@ -368,7 +431,7 @@ Account.deposit = function (accountNumber, amount) {
 const withdrawAccountInput = document.querySelector('#withdraw-account-input');
 const withdrawName = document.querySelector('#withdraw-name');
 //event listener on user input: display account name if valid
-withdrawAccountInput.addEventListener('input', (e) => {
+withdrawAccountInput.addEventListener('input', () => {
     let withdrawAccountName;
     //if input has no value
     if (!withdrawAccountInput.value) {
@@ -396,7 +459,7 @@ withdrawForm.addEventListener('submit', (e) => {
     Account.withdraw(accountNumberValue, withdrawAmount);
 })
 
-Account.withdraw = function (accountNumber, amount) {
+Account.withdraw = (accountNumber, amount) => {
     accountNumber = Number(accountNumber);
     amount = parseFloat(amount);
     const accounts = AccountStore.getAccouts();
@@ -409,14 +472,14 @@ Account.withdraw = function (accountNumber, amount) {
         };
     }
     if (withdrawAccount) {
-        if (balance >= amount) {
+        let isBalanceSufficient = checkBalance(balance, amount);
+        if (isBalanceSufficient) {
             withdrawName.innerText = "";
-            alert(`Successfully withdrew ₱${amount} from ${withdrawAccount}`);
-            // update account balance in local storage
+            alertMessage.textContent = `Successfully withdrew ₱${amount} from ${withdrawAccount}`;
+            success();
+            // alert(`Successfully withdrew ₱${amount} from ${withdrawAccount} `);
             AccountStore.withdraw(accountNumber, amount);
             AccountUI.clear_inputs();
-        } else {
-            alert(`INSUFFICIENT BALANCE: Account has ₱${balance} balance`);
         }
     } else {
         //check if length is just wrong or if account really does not exist
@@ -434,7 +497,7 @@ let senderName = document.querySelector('#sender-name');
 let receiverName = document.querySelector('#receiver-name');
 
 //event listener on user input: display account name if valid
-senderAccountInput.addEventListener('input', (e) => {
+senderAccountInput.addEventListener('input', () => {
     let senderAccountName;
     //if input has no value
     if (!senderAccountInput.value) {
@@ -454,7 +517,7 @@ senderAccountInput.addEventListener('input', (e) => {
     }
 })
 
-receiverAccountInput.addEventListener('input', (e) => {
+receiverAccountInput.addEventListener('input', () => {
     let receiverAccountName;
     //if input has no value
     if (!receiverAccountInput.value) {
@@ -483,12 +546,13 @@ sendForm.addEventListener('submit', (e) => {
     let sendAmount = document.querySelector('#send-amount-input').value;
     Account.send(senderAccountNumberValue, receiverAccountNumberValue, sendAmount);
 })
-Account.send = function (from_accountNumber, to_accountNumber, amount) {
+Account.send = (from_accountNumber, to_accountNumber, amount) => {
     let senderAccount;
-    let receiverAccount;
     let senderAccountName;
-    let receiverAccountName;
     let senderBalance;
+    let receiverAccount;
+    let receiverAccountName;
+
     amount = parseFloat(amount);
     const accounts = AccountStore.getAccouts();
     //check if sender and receiver accounts exist
@@ -503,39 +567,95 @@ Account.send = function (from_accountNumber, to_accountNumber, amount) {
         };
     }
     if (senderAccount && receiverAccount) {
-        if (senderBalance >= amount) {
+        let isBalanceSufficient = checkBalance(senderBalance, amount);
+        if (isBalanceSufficient) {
             senderName.innerText = "";
             receiverName.innerText = "";
-            alert(`Successfully sent ₱${amount} from ${senderAccountName}: ${senderAccount} to ${receiverAccountName}: ${receiverAccount}`);
-            //update local storage
+            alertMessage.textContent = `Successfully sent ₱${amount} from ${senderAccountName}: ${senderAccount} to ${receiverAccountName}: ${receiverAccount} `;
+            success();
             AccountStore.send(senderAccount, receiverAccount, amount);
-            //todo update UI
             AccountUI.clear_inputs();
-        } else {
-            alert(`INSUFFICIENT BALANCE: Sender has ₱${senderBalance} balance`)
         }
-
     } else if (!senderAccount && receiverAccount) {
-        alert('Sender does not exist');
+        alertMessage.textContent = 'INVALID ACCOUNT: Sender does not exist';
+        invalid();
+        // alert('Sender does not exist');
     } else if (senderAccount && !receiverAccount) {
-        alert('Receiver does not exist');
+        alertMessage.textContent = 'INVALID ACCOUNT: Receiver does not exist';
+        invalid();
+
+        // alert('Receiver does not exist');
     } else {
-        alert('Invalid Accounts');
+        alertMessage.textContent = 'INVALID ACCOUNTS';
+        invalid();
+
+        // alert('Invalid Accounts');
     }
     //reset sender and receiver details to prevent override
     senderAccount = undefined;
     receiverAccount = undefined;
 }
-//clears local storage
-document.getElementById('clear-storage').addEventListener('click', () => {
-    localStorage.clear();
+
+//*DELETE AN ACCOUNT 
+//select the account table and delete via 'event propagation' - target the actual list
+document.querySelector('#account-list-data').addEventListener('click', (e) => {
+    // traverse the DOM to get the targetAccountNumber
+    let targetAccount = e.target.parentElement.parentElement.firstElementChild.textContent;
+    //remove spaces between string target account number
+    let targetAccountNumber = Number(targetAccount.replace(/\s/g, ''))
+    //remove account from storage. 
+    AccountStore.deleteAccount(targetAccountNumber);
+    //remove account from list
+    AccountUI.deleteAccount(e.target);
 })
-//checks if account number is 12 digits
+
+//*HELPER FUNCTIONS
+
+//todo use helper functions for each form
+function findAccountNumber(accountNumber) {
+    const accounts = AccountStore.getAccouts();
+    accounts.forEach(account => {
+        if (account.accountNumber === accountNumber) {
+            return accountNumber;
+        } else {
+            return false;
+        }
+    })
+};
+function findAccountName(accountNumber) {
+    const accounts = AccountStore.getAccouts();
+    accounts.forEach(account => {
+        if (account.accountNumber === accountNumber) {
+            return account.name;
+        } else {
+            return false;
+        }
+    })
+}
 function checkNumberLength(accountNumber) {
     if (String(accountNumber).length != 12) {
-        alert(`INVALID ACCOUNT:
-Account number must contain 12 digits. Current input contains ${String(accountNumber).length} digits`);
+        alertMessage.textContent = `INVALID ACCOUNT:
+Account number must contain 12 digits. Current input contains ${String(accountNumber).length} digits`;
     } else {
-        alert('ACCOUNT NOT FOUND');
+        alertMessage.textContent = `INVALID ACCOUNT: ACCOUNT NOT FOUND`;
+    }
+    invalid();
+}
+function checkBalance(currentBalance, amount) {
+    if (currentBalance >= amount) {
+        return currentBalance;
+    } else {
+        alertMessage.textContent = `INSUFFICIENT BALANCE: Account only has ₱${currentBalance} balance`;
+        invalid();
+        return false;
     }
 }
+function success() {
+    removeAlert.style.display = 'block';
+    alertBox.style.backgroundColor = 'var(--success)';
+}
+function invalid() {
+    removeAlert.style.display = 'block';
+    alertBox.style.backgroundColor = 'var(--invalid)';
+}
+
